067b0c29280cf70c59733a9b844938362789cbef
Thiago Santana <tnsantana@gmail.com>
2020-03-14 14:16:18 -0300

    Project 3 Contracts Coffee Access Control

--- project3/contracts/coffeeaccesscontrol/RetailerRole.sol
+++ project3/contracts/coffeeaccesscontrol/RetailerRole.sol
@@ -1,48 +1,45 @@
-pragma solidity ^0.4.24;
+pragma solidity >=0.4.24;
 
 // Import the library 'Roles'
 import "./Roles.sol";
 
 // Define a contract 'RetailerRole' to manage this role - add, remove, check
 contract RetailerRole {
+  using Roles for Roles.Role;
 
-  // Define 2 events, one for Adding, and other for Removing
-  
-  // Define a struct 'retailers' by inheriting from 'Roles' library, struct Role
+  event RetailerAdded(address indexed account);
+  event RetailerRemoved(address indexed account);
 
-  // In the constructor make the address that deploys this contract the 1st retailer
+  Roles.Role private ratailers;
+
   constructor() public {
-    
+    _addRetailer(msg.sender);
   }
 
-  // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyRetailer() {
-    
+    require(isRetailer(msg.sender), "Not a Retailer");
     _;
   }
 
-  // Define a function 'isRetailer' to check this role
   function isRetailer(address account) public view returns (bool) {
-    
+    return ratailers.has(account);
   }
 
-  // Define a function 'addRetailer' that adds this role
   function addRetailer(address account) public onlyRetailer {
-    
+    _addRetailer(account);
   }
 
-  // Define a function 'renounceRetailer' to renounce this role
   function renounceRetailer() public {
-    
+    _removeRetailer(msg.sender);
   }
 
-  // Define an internal function '_addRetailer' to add this role, called by 'addRetailer'
   function _addRetailer(address account) internal {
-    
+    ratailers.add(account);
+    emit RetailerAdded(account);
   }
 
-  // Define an internal function '_removeRetailer' to remove this role, called by 'removeRetailer'
   function _removeRetailer(address account) internal {
-    
+    ratailers.remove(account);
+    emit RetailerRemoved(account);
   }
 }
